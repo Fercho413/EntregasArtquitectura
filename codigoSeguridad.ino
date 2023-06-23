f743c1188d243bb20c72c9176ae2f28d597185d2
Santiago F <80357562+Fercho413@users.noreply.github.com>
2023-06-23 16:17:09 -0500

    Update codigoSeguridad.ino

--- codigoSeguridad.ino
+++ codigoSeguridad.ino
@@ -20,10 +20,10 @@ const byte KEYPAD_COLS = 4;
 byte rowPins[KEYPAD_ROWS] = {5, 4, 3, 2};
 byte colPins[KEYPAD_COLS] = {A3, A2, A1, A4};
 char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
-  {'1', '2', '3', '+'},
-  {'4', '5', '6', '-'},
-  {'7', '8', '9', '*'},
-  {'.', '0', '=', '/'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
